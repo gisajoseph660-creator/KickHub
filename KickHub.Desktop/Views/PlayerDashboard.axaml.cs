c1444b46674dc331df80289918760303c0aba5fb
gisajoseph660 <gisajoseph660@gmail.com>
2026-08-27 16:22:17 +0200

    Add player dashboard

--- KickHub.Desktop/Views/PlayerDashboard.axaml.cs
+++ KickHub.Desktop/Views/PlayerDashboard.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace KickHub.Desktop.Views;
 
@@ -7,5 +8,10 @@ public partial class PlayerDashboard : Window
     public PlayerDashboard()
     {
         InitializeComponent();
+    }
+
+    private void ViewResults_Click(object? sender, RoutedEventArgs e)
+    {
+        MessageText.Text = "Recent result: KickHub United 2 - 1 Berlin FC";
     }
 }
